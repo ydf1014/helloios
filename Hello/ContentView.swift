7231ff3f2a1f3c458940aa534b90ce4007e97b54
Danfeng Yang <ydf1014@uw.edu>
2021-04-06 15:59:43 -0700

    Initial Commit

---
+++ Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by Danfeng Yang on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Danfeng Yang on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
